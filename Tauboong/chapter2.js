26489a696f5147b36e3a494e137c3c3d068c2a38
boong <taurusboong2@gmail.com>
2022-10-18 22:26:51 +0900

    boong: 4장 시작

---
+++ Tauboong/chapter2.js
@@ -0,0 +1,14 @@
+/**
+ * 변수에는 매우 다양한 '값'들을 담을 수가 있다.
+ */
+const favoriteDrink = true;
+
+const getDrink = favoriteDrink ? '코카콜라' : '제로펩시';
+
+const myHommies = ['김씨', '안씨', '박씨', '이씨'];
+
+const today = new Date();
+
+const add = (num1, num2) => num1 + num2;
+
+const plusOneTwo = 1 + 2;
