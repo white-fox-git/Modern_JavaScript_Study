77320ed97d6db5b3fc5365aea7ea2ffbc6594bab
boong <taurusboong2@gmail.com>
2022-10-18 22:41:26 +0900

    boong: 221018

--- Tauboong/chapter2.js
+++ Tauboong/chapter2.js
@@ -1,6 +1,6 @@
-/**
- * 변수에는 매우 다양한 '값'들을 담을 수가 있다.
- */
+let hi;
+
+/* 변수에는 매우 다양한 '값'들을 담을 수가 있다. */
 const favoriteDrink = true;
 
 const getDrink = favoriteDrink ? '코카콜라' : '제로펩시';
@@ -12,3 +12,10 @@ const today = new Date();
 const add = (num1, num2) => num1 + num2;
 
 const plusOneTwo = 1 + 2;
+
+/* 변수는 무조건 선언을 하고 사용을 해야 한다. */
+const JS = 'JavaScript';
+
+var TS = 'TypeScript';
+
+let rq = 'React-query';
